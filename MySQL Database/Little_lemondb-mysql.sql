0685dbcdfa8eb7478cdde2e51c5860fa8a9e74af
Saad711T <75455552+Saad711T@users.noreply.github.com>
2025-02-26 00:19:46 +0300

    Project

--- MySQL Database/Little_lemondb-mysql.sql
+++ MySQL Database/Little_lemondb-mysql.sql
@@ -2,7 +2,7 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
+-- Host: localhost
 -- Generation Time: Feb 25, 2025 at 09:12 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
